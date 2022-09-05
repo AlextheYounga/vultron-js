e9e1b2c44e9002e41ec4e9a164dc5c745e363437
Alex Younger <alex@lucriv.com>
2022-09-05 16:40:10 -0400

    refactor: move directories to be more rails/laravel

--- app/server/controllers/auth_controller.js
+++
@@ -1,9 +0,0 @@
-const Auth = require('../../middleware/auth/auth')
-
-const AuthController = {
-	login: async function (params) {
-		return Auth.login(params)
-	},
-}
-
-module.exports = AuthController
